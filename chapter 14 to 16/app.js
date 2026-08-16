1175cbac0c419de33db16d0f210647fe94e2266e
Ansharahcoder <syedaansharah88@gmail.com>
2026-08-16 14:10:45 +0500

    javascript assignment 6 complete

--- chapter 14 to 16/app.js
+++ chapter 14 to 16/app.js
@@ -139,4 +139,5 @@ document.write("<option>" + phones[2] + "</option>");
 document.write("<option>" + phones[3] + "</option>");
 document.write("<option>" + phones[4] + "</option>");
 document.write("<option>" + phones[5] + "</option>");
-document.write("<select>");
+
+document.write("</select>");
